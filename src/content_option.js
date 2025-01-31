9a03020e05f43ed04cce38597ec3c8f5526ffa06
Syed Rizwan K <syedrizwank272@gmail.com>
2025-01-31 18:14:05 +0530

    first edit

--- src/content_option.js
+++ src/content_option.js
@@ -1,59 +1,53 @@
-const logotext = "JOHN";
+const logotext = "RIZWAN";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Syed Rizwan k",
+    description: "I’m Syed Rizwan k Full stack devloper,currently working in Banglore",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Syed Rizwan",
     animated: {
-        first: "I love coding",
+        first: "Developer",
         second: "I code cool websites",
         third: "I develop mobile apps",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    description: "i crafts responsive websites where technologies meet creativity",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
     title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    aboutme: "Solution-oriented developer with experience in building robust software systems and data-driven dashboards. Skilled in performance optimization, scalable architecture, and effective collaboration. Eager to apply my expertise in delivering impactful, user-focused solutions",
 };
 const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
+        jobtitle: "Associate softwere developer",
+        where: "Trimixel",
+        date: "2024",
     },
 ];
 
 const skills = [{
-        name: "Python",
+        name: "Javascript",
         value: 90,
     },
     {
-        name: "Djano",
+        name: "Java",
         value: 85,
     },
     {
-        name: "Javascript",
+        name: "SQL",
         value: 80,
     },
     {
         name: "React",
-        value: 60,
+        value: 90,
     },
     {
-        name: "Jquery",
+        name: "HTML",
+        value: 85,
+    },
+    {
+        name: "CSS",
         value: 85,
     },
 ];
@@ -64,10 +58,6 @@ const services = [{
     },
     {
         title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Wordpress Design",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
     },
 ];
@@ -136,20 +126,20 @@ const dataportfolio = [{
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_EMAIL: "syedrizwank271@gmail.com",
+    YOUR_FONE: "7829673740",
+    description: "------------------ ",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+    YOUR_SERVICE_ID: "Rizz@u.ai",
+    YOUR_TEMPLATE_ID: "----",
+    YOUR_USER_ID: "----",
 };
 
 const socialprofils = {
-    github: "https://github.com",
+    github: "https://github.com/userisRizz",
     facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
+    linkedin: "https://www.linkedin.com/in/syed-rizwan-sadiq/",
     twitter: "https://twitter.com",
 };
 export {
